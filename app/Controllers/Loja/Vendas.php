c91b949c21b58baa3d271ef488b9a9971dde5142
OliveiraRafaella <rafaella.vitorino2001@gmail.com>
2022-09-12 16:51:08 -0300

    primeiro commit

---
+++ app/Controllers/Loja/Vendas.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers\Loja;
+
+use CodeIgniter\Controller;
+
+class Vendas extends Controller //BaseController
+{
+    public function index()
+    {
+        echo 'inicio';    
+    }
+    
+}
---
+++ app/Controllers/Main.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\HTTP\Request;
+use CodeIgniter\Controller;
+
+class Main extends Controller //BaseController
+{
+    public function index()
+    {
+        //$data ['frase'] = "Rafa";
+        echo 'inicio';    
+    }
+}
